5ac56fec7e69a6d303b18c1ca34c699a4cd3bc8b
nimamasoumi <mraghahe@protonmail.com>
2023-12-22 17:40:03 -0500

    Extending the code to inlucde more features

--- ConnectionControl.java
+++ ConnectionControl.java
@@ -1,5 +1,7 @@
 public class ConnectionControl
 {
+    public ConnectionControl(){}
+    private NetworkConfig NetConfig = new NetworkConfig();
     public static void main(String[] args)
     {
         // arg[0] is the ip address
@@ -7,6 +9,6 @@ public class ConnectionControl
         String hostname = args[0];
         int port = Integer.parseInt(args[1]);
 
-        System.out.printf("\n %s %d\n",hostname, port);
+        System.out.printf("\nConnecting to %s using port %d\n",hostname, port);
     }
 }
