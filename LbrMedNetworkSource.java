67ce021541e25c2262859407e20d241eb39214db
nimamasoumi <mraghahe@protonmail.com>
2024-01-16 19:37:20 -0500

    Adding fields to facilititate the connection to the server

--- LbrMedNetworkSource.java
+++ LbrMedNetworkSource.java
@@ -1,6 +1,56 @@
+import java.net.Socket;
+
 public class LbrMedNetworkSource
 {
-    public boolean Connect()
+    private Socket tcpClient;
+    private String IP = "";
+    private int Port = 0;
+    private boolean EnableExternalControl = true;
+    
+    public void setExternalControl(boolean _ed)
+    {
+        this.EnableExternalControl = _ed;
+    }
+    public boolean getExternalControl()
+    {
+        return this.EnableExternalControl;
+    }
+    public void setIP(String _IP)
+    {
+        this.IP = _IP;
+    }
+    public String getIP()
+    {
+        return this.IP;
+    }
+    public void setPort(int _port)
+    {
+        this.Port = _port;
+    }
+    public int getPort()
+    {
+        return this.Port;
+    }
+
+    public boolean Connect(NetworkConfig _nc)
+    {
+        if(tcpClient.isConnected())
+        {
+            System.out.println("Already Connected!");
+            return false;
+        }
+
+        this.IP = _nc.Hostname;
+        this.Port = _nc.Port;
+        
+        if(this.EnableExternalControl)
+        {
+
+        }
+        return false;
+    }
+
+    private boolean InitilizeExternalControl(NetworkConfig _nc)
     {
         return false;
     }
