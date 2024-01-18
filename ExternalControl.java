32ea67e1b5c710cd4811781485fbc43b4bdcf63e
nimamasoumi <mraghahe@protonmail.com>
2024-01-18 18:57:57 -0500

    Adding UDP socket initilization and closing

--- ExternalControl.java
+++ ExternalControl.java
@@ -1,4 +1,33 @@
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+import javax.swing.JTextArea;
+
 public class ExternalControl
-{
+{    
     public ExternalControl(){}
+    private DatagramSocket socket = null;
+    private JTextArea logger = null;
+
+    public void setLogger(JTextArea _logger)
+    {
+        this.logger = _logger;
+    }
+    public void Open(String _robotIP)
+    {
+        Close();
+        try{
+            socket = new DatagramSocket();
+        }
+        catch(SocketException e)
+        {
+            this.logger.append("[ExternalControl] Problem opening a UDP socket.\n");
+            return;
+        }
+        
+    }
+    public void Close()
+    {
+
+    }
 }
