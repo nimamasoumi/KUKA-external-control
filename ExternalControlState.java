235e069e51509e7ae8047b0e04cf59a1f64f9cce
nimamasoumi <mraghahe@protonmail.com>
2024-01-18 18:57:12 -0500

    Expanding the method BeginControl by setting the extrnal controller

--- ExternalControlState.java
+++ ExternalControlState.java
@@ -1,9 +1,29 @@
+import javax.swing.JTextArea;
+
 public class ExternalControlState {
     public ExternalControlState(){}
     private ExternalControl controller = new ExternalControl();
+    private boolean active = false;
+    private JTextArea logger = null;
+    public boolean isActive()
+    {
+        return this.active;
+    }
+    public void setLogger(JTextArea _logger)
+    {
+        this.logger = _logger;
+    }
 
     public boolean BeginControl(String _hostname)
-    {
+    {        
+        if (_hostname=="")
+        {
+            this.logger.append("[ExternalControlState] Hostname cannot be empty!\n");
+            return false;
+        }
+        controller.setLogger(logger);
+        controller.Open(_hostname);
+        
         return true;
     }
 }
