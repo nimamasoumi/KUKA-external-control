0660facae5dd3f393fc8448cd030c7eb943611d0
nimamasoumi <mraghahe@protonmail.com>
2024-01-19 19:06:28 -0500

    Completing the Open() method to create a client UDP socket

--- ExternalControl.java
+++ ExternalControl.java
@@ -1,6 +1,4 @@
 import java.net.DatagramSocket;
-import java.net.SocketException;
-
 import javax.swing.JTextArea;
 
 public class ExternalControl
@@ -8,6 +6,14 @@ public class ExternalControl
     public ExternalControl(){}
     private DatagramSocket socket = null;
     private JTextArea logger = null;
+    
+    /*  
+    Local port to open and communicate with the robot. This should be the same port number 
+    as set in the status receivers setting in the robot project properties under ExternalControl
+    */
+    private final int LOCAL_PORT = 30333;
+    private final int SOCKET_TIMEOUT = 50;
+    private long clientCounter = 0;
 
     public void setLogger(JTextArea _logger)
     {
@@ -17,17 +23,22 @@ public class ExternalControl
     {
         Close();
         try{
-            socket = new DatagramSocket();
+            socket = new DatagramSocket(LOCAL_PORT);
+            socket.setSoTimeout(SOCKET_TIMEOUT);
         }
-        catch(SocketException e)
+        catch(Exception e)
         {
             this.logger.append("[ExternalControl] Problem opening a UDP socket.\n");
             return;
         }
-        
+        clientCounter = 0;
     }
     public void Close()
     {
-
+        if (socket != null)
+        {
+            socket.close();
+        }
+        socket = null;
     }
 }
