354dbcf690a34e77155b1026db214c3751fbfaf4
nimamasoumi <mraghahe@protonmail.com>
2024-01-23 19:56:04 -0500

    Storing the KUKA robot IP address

--- ExternalControl.java
+++ ExternalControl.java
@@ -1,4 +1,5 @@
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import javax.swing.JTextArea;
 
 public class ExternalControl
@@ -6,12 +7,14 @@ public class ExternalControl
     public ExternalControl(){}
     private DatagramSocket socket = null;
     private JTextArea logger = null;
+    private InetAddress ip = null;
     
     /*  
     Local port to open and communicate with the robot. This should be the same port number 
     as set in the status receivers setting in the robot project properties under ExternalControl
     */
     private final int LOCAL_PORT = 30333;
+    private final int ROBOT_EXT_CTRL_PORT = 30300;
     private final int SOCKET_TIMEOUT = 50;
     private long clientCounter = 0;
 
@@ -25,6 +28,7 @@ public class ExternalControl
         try{
             socket = new DatagramSocket(LOCAL_PORT);
             socket.setSoTimeout(SOCKET_TIMEOUT);
+            ip = InetAddress.getByName(_robotIP);
         }
         catch(Exception e)
         {
