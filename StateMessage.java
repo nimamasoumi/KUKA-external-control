290c7dc16adbbb8422421b39a0567e9fa5da0979
nimamasoumi <mraghahe@protonmail.com>
2024-01-31 18:43:06 -0500

    Data Storage class for the External Control Messages

---
+++ StateMessage.java
@@ -0,0 +1,11 @@
+public class StateMessage 
+{
+    /// Data storage class for 'External Control UDP State Messages'.
+    /// These type of messages are sent out by the robot as answer messages to any 
+    /// command message received on its External Control UDP interface 
+    /// and contain a header, status signals, current state of the default application, 
+    /// and the current values of the robot's inputs App_Start and App_Enable.
+
+    private StateMessage(){}
+    
+}
